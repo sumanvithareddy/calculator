adf0b0506f842e94a84e16307f80d7c36e91f990
kayam.Sumanvitha reddy <anvichinnu092007@gmail.com>
2026-06-28 17:49:00 +0530

    Implement basic calculator functionality in script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+// Keyboard Support
+document.addEventListener("keydown",function(event){
+
+    const key = event.key;
+
+    if((key>='0' && key<='9') ||
+        ['+','-','*','/','.','%'].includes(key)){
+        appendValue(key);
+    }
+
+    else if(key==="Enter"){
+        calculate();
+    }
+
+    else if(key==="Backspace"){
+        deleteLast();
+    }
+
+    else if(key==="Escape"){
+        clearDisplay();
+    }
+});
